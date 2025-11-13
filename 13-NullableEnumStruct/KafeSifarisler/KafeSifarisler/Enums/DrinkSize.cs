a2f422fed00fd4019f21209040c1620925997d68
feridhsnzad <hsnzadfrid68@.com>
2025-11-14 00:10:24 +0400

    kafe sifaris

---
+++ 13-NullableEnumStruct/KafeSifarisler/KafeSifarisler/Enums/DrinkSize.cs
@@ -0,0 +1,9 @@
+﻿namespace CafeOrderSystem.Enums
+{
+    public enum DrinkSize
+    {
+        Small = 0,
+        Medium = 1,
+        Large = 2
+    }
+}
---
+++ 13-NullableEnumStruct/KafeSifarisler/KafeSifarisler/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace CafeOrderSystem.Enums
+{
+    public enum OrderStatus
+    {
+        New = 0,
+        Preparing = 1,
+        Ready = 2,
+        Delivered = 3
+    }
+}
