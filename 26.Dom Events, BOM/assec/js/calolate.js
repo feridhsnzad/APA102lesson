586d7cbf62a29325236153dcf5ad25880f3527d2
feridhsnzad <hsnzadfrid68@gmail.com>
2025-11-27 00:29:18 +0400

    Add files via upload

---
+++ 26.Dom Events, BOM/assec/js/calolate.js
@@ -0,0 +1,14 @@
+let inputone = document.querySelector(".input1")
+let inputtwo = document.querySelector(".input2")
+
+let result = document.querySelector(".expul")
+
+let plus = document.querySelector("btn_plus")
+
+
+
+let topla = ()=>{
+    if () {
+        
+    }
+}
