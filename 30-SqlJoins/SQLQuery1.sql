c4c19195b0f428c91ea6a1eb810830ce041fe158
feridhsnzad <hsnzadfrid68@gmail.com>
2025-12-05 05:44:24 +0400

    Add files via upload

---
+++ 30-SqlJoins/SQLQuery1.sql
@@ -0,0 +1,30 @@
+create database CompanyDB
+use CompanyDB 
+
+
+create table Countries(
+Id int primary key identity,
+Name nvarchar (20),
+)
+
+
+create table Cityes(
+Id int primary key identity,
+Name nvarchar (20),
+CountriId INT FOREIGN KEY REFERENCES Countries(Id)
+)
+
+
+create table Employees(
+Id int primary key identity,
+Name nvarchar (20) ,
+Surname nvarchar (20) ,
+Age int ,
+Salary decimal(10,2),
+Position nvarchar(50),
+CityId INT FOREIGN KEY REFERENCES Cityes(Id),
+IsDeleted bit
+)
+
+
+
