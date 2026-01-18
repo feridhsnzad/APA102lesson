c7b668b863a5d5af045c0e27fc83360b04b378ff
feridhsnzad <hsnzadfrid68@gmail.com>
2026-01-18 20:15:31 +0400

    Delete 38.Category Update-Delete, Slider Index directory

--- 38.Category Update-Delete, Slider Index/Models/Base/BaseEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrontToBack.Models
-{
-    public abstract class BaseEntity
-    {
-        public int Id { get; set; }
-        public bool IsDeleted { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-    }
-}
--- 38.Category Update-Delete, Slider Index/Models/Shipping.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FrontToBack.Models
-{
-    public class Shipping:BaseEntity
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string UrlImage { get; set; }
-    }
-}
